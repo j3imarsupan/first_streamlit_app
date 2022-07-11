4e5cb9db42ca03c01de6a7c7461a8ad4d3eab01b
Jeimar Supan <j3imarsupan@gmail.com>
2022-07-11 23:07:13 +0800

    add try-catch on fruityvice section

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,29 +19,30 @@ my_fruit_list = pd.read_csv(
 
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# Let's put a pick list here so they can pick the fruit they want to include
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(
     my_fruit_list.index), ['Avocado', 'Strawberries'])
 
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-# Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input(
-    'What fruit would you like information about?', 'Kiwi'
-)
-streamlit.write('The user entered ', fruit_choice)
 
-fruityvice_response = requests.get(
-    "https://fruityvice.com/api/fruit/watermelon")
-# streamlit.text(fruityvice_response.json())
+try:
+    fruit_choice = streamlit.text_input(
+        'What fruit would you like information about?')
+    if not fruit_choice:
+        streamlit.error("Please select a fruit to get information.")
+    else:
+        fruityvice_response = requests.get(
+            "https://fruityvice.com/api/fruit/watermelon")
 
-# convert json response into a pandas dataframe
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# display dataframe
-streamlit.dataframe(fruityvice_normalized)
+        fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+        # display dataframe
+        streamlit.dataframe(fruityvice_normalized)
+
+except URLError as e:
+    streamlit.error()
 
 # pause here for investigation purposes
 streamlit.stop()
